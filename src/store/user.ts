14f0645015646c55192a7ed4cd2b840165db5bbf
guyi <1802366955@qq.com>
2024-03-16 23:25:09 +0800

    用户状态管理

---
+++ src/store/user.ts
@@ -0,0 +1,26 @@
+// 初始化状态
+import { StoreOptions } from "vuex";
+
+export default {
+  namespace: true,
+  // 执行异步操作, 并触发 mutation 的更改(action 调用 mutation)
+  state: () => ({
+    loginUser: {
+      username: "未登录",
+      role: "notLogin",
+    },
+  }),
+  // 执行异步操作, 并触发 mutation 的更改(action 调用 mutation)
+  actions: {
+    // todo 远程登录
+    getLoginUser({ commit, state }, payload) {
+      commit("updateUser", { userName: "孤诣" });
+    },
+  },
+  // 定义对变量进行更新操作的方法(尽量同步)
+  mutations: {
+    updateUser(state, payload) {
+      state.loginUser = payload;
+    },
+  },
+} as StoreOptions<any>;
