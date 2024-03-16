6ea146eee907dbcdd2ceab168d22088c3b9692bd
guyi <1802366955@qq.com>
2024-03-16 23:19:01 +0800

    初步定义权限管理

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,20 @@
 </style>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
+const router = useRouter();
+const store = useStore();
+
+router.beforeEach((to, from, next) => {
+  // 仅管理员可见
+  if (to.meta?.access === "canAdmin") {
+    if (store.state.user.loginUser?.role !== "admin") {
+      next("/noAuth");
+      return;
+    }
+  }
+  next();
+});
 </script>
