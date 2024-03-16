4a0505ac4e925c2e9644ded147557d0e508d6068
guyi <1802366955@qq.com>
2024-03-16 23:20:40 +0800

    页面基础布局

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-footer class="header">
+        <GlobalHeader />
+      </a-layout-footer>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">底部</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style>
+#basicLayout {
+}
+
+#basicLayout .header {
+  margin-bottom: 16px;
+  box-shadow: #eee 1px 1px 5px;
+}
+
+#basicLayout .content {
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background-color: #eeeeee;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script>
+import GlobalHeader from "@/components/GlobalHeader";
+
+export default {
+  components: { GlobalHeader },
+};
+</script>
