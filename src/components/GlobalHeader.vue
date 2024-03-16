81bcc863851ddd43f34b77f2a5f6a4e6e5ec9812
guyi <1802366955@qq.com>
2024-03-16 23:21:15 +0800

    全局菜单栏

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,81 @@
+<template>
+  <a-row class="globalHeader" style="margin-bottom: 16px" align="center">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.png" alt="图片地址错误" />
+            <div class="logo-text">GOJ</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in routes" :key="item.path"
+          >{{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>{{ store.state.user?.loginUser?.username ?? "未登录" }}</div>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const router = useRouter();
+
+// 默认主页
+const selectedKeys = ref(["/"]);
+
+// 路由跳转后, 更新选中的菜单项
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+/**
+ * 跳转到对应菜单项的页面
+ * @param key - 路由
+ */
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+
+const store = useStore();
+
+// setTimeout(() => {
+//   store.dispatch("user/getLoginUser", {
+//     username: "孤诣",
+//     role: "admin",
+//   });
+// }, 3000);
+</script>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  height: 48px;
+}
+
+.logo-text {
+  font-size: 35px;
+  color: black;
+  margin-left: 7px;
+}
+</style>
