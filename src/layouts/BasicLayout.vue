2d3e4d2e85b85d672e26ff2d17fa0be0867be787
guyi <1802366955@qq.com>
2024-03-17 14:12:25 +0800

    优化页面

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="basicLayout">
-    <a-layout style="height: 400px">
+    <a-layout style="min-height: 100vh">
       <a-layout-footer class="header">
         <GlobalHeader />
       </a-layout-footer>
@@ -23,12 +23,13 @@
 
 #basicLayout .content {
   margin-bottom: 16px;
+  padding: 20px;
 }
 
 #basicLayout .footer {
   background-color: #eeeeee;
   padding: 16px;
-  position: absolute;
+  position: sticky;
   bottom: 0;
   left: 0;
   right: 0;
