e2e0e1447cce9cd5792c43cf0de532d22663a9db
guyi <1802366955@qq.com>
2024-03-17 14:25:29 +0800

    不将隐藏页面显示在菜单上

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -16,7 +16,7 @@
             <div class="logo-text">GOJ</div>
           </div>
         </a-menu-item>
-        <a-menu-item v-for="item in routes" :key="item.path"
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path"
           >{{ item.name }}
         </a-menu-item>
       </a-menu>
@@ -34,6 +34,14 @@ import { ref } from "vue";
 import { useStore } from "vuex";
 
 const router = useRouter();
+
+// 展示在菜单的路由数组
+const visibleRoutes = routes.filter((item, index) => {
+  if (item.meta?.hideInMenu) {
+    return false;
+  }
+  return true;
+});
 
 // 默认主页
 const selectedKeys = ref(["/"]);
