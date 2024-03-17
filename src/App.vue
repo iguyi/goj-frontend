37e27f879d3454f59d7c429dab557bb37dc3bc5e
guyi <1802366955@qq.com>
2024-03-17 16:29:50 +0800

    预留项目入口函数

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,18 @@
 import BasicLayout from "@/layouts/BasicLayout";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+import { onMounted } from "vue";
+
+/**
+ * 全局初始化函数, 全局单次调用的代码可以写到这里
+ */
+const doInit = () => {
+  console.log("欢迎使用 GOJ 项目");
+};
+
+onMounted(() => {
+  doInit();
+});
 
 const router = useRouter();
 const store = useStore();
