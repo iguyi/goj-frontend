ded5676f17d5f7785814493bf1e0d38f33865dae
guyi <1802366955@qq.com>
2024-03-25 14:38:08 +0800

    引入权限管理（自定义）

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import router from "./router";
 import store from "./store";
 import ArcoVue from "@arco-design/web-vue";
 import "@arco-design/web-vue/dist/arco.css";
+import "@/access"; // 权限管理
 
 createApp(App).use(store).use(router).use(ArcoVue).mount("#app");
