fff07060fa24c98b93a891a2ae8be3137a000fe0
guyi <1802366955@qq.com>
2024-03-25 14:42:28 +0800

    -远程获取登录用户信息 -为 currentUser 新增用于标识用户是否尝试登录过的属性

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,6 +1,7 @@
 // 初始化状态
 import { StoreOptions } from "vuex";
 import AccessEnum from "@/access/accessEnum";
+import { UserControllerService } from "../../generated";
 
 export default {
   namespace: true,
@@ -9,13 +10,23 @@ export default {
     currentUser: {
       username: "未登录",
       userRole: AccessEnum.NOT_LOGIN,
+      flag: false, // false 表示用户为尝试过登录
     },
   }),
   // 执行异步操作, 并触发 mutation 的更改(action 调用 mutation)
   actions: {
-    // todo 远程登录
-    getCurrentUser({ commit, state }, payload) {
-      commit("updateUser", payload);
+    // 从远程获取用户登录信息
+    async getCurrentUser({ commit, state }, payload) {
+      const res = await UserControllerService.getLoginUserUsingGet();
+      if (res.code === 0) {
+        commit("updateUser", res.data);
+      } else {
+        commit("updateUser", {
+          ...state.currentUser,
+          userRole: AccessEnum.NOT_LOGIN,
+          flag: true,
+        });
+      }
     },
   },
   // 定义对变量进行更新操作的方法(尽量同步)
